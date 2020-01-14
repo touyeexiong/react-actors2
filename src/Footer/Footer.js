670ee823344c41e0eccb0df7a938c42a6f4734f5
Tou Xiong <touyeexiong@gmail.com>
2020-01-14 11:00:00 -0600

    everything working now

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,4 +1,4 @@
-import react, { Component } from 'react';
+import React, { Component } from 'react';
 
 class Footer extends Component {
     render() {
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
         <header>
-            <h1>Mission Control</h1>
+            <h1>React Actors</h1>
         </header>
         );
     }
